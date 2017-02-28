efb583625515681d442fc1983890535c5552131b
Jonathan <gamma_chen@yahoo.com.tw>
2017-02-28 20:56:20 +0800

    resume

--- js/resume.js
+++ js/resume.js
@@ -51,3 +51,50 @@ function drawSkillBarChart() {
 
   };
 }
+
+function drawExperiences() {
+  var xLine = 150;
+  var px = [20, 75, 100, 140, 160];
+  var c=document.getElementById("canvas2");
+  var ctx=c.getContext("2d");
+  
+  ctx.font="12px Arial";
+  
+  // Show the different textAlign values
+  ctx.textAlign="right";
+  ctx.fillText("November 2016",xLine-10,px[0]+5);
+  ctx.fillText("March 2013",xLine-10,px[1]+5);
+  ctx.fillText("August 2012",xLine-10,px[2]+5);
+  ctx.fillText("September 2004",xLine-10,px[3]+5);
+  ctx.fillText("September 2004",xLine-10,px[4]+5);
+  
+  ctx.textAlign="left";
+  ctx.fillText("Senior software engineer in Marvell",xLine+10,px[0]+15);
+  ctx.fillText("llvm optimization for ARM",xLine+20,px[0]+2*15);
+  ctx.fillText("The simulator programmer of Marvell's ARM SOC chips",xLine+20,px[0]+3*15);
+  
+  ctx.fillText("Programmer in llvm (a compiler) open source team",xLine+10,px[1]+15);
+  
+  ctx.fillText("Senior software engineer in Mortorola",xLine+10,px[2]+15);
+  ctx.fillText("Software engineer in Set Top Box TV",xLine+20,px[2]+2*15);
+  
+  ctx.fillText("Software engineer in a few Taiwan companies",xLine+10,px[3]+15);
+  
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(xLine,0);
+  ctx.lineTo(xLine,canvas2.height-1);
+  ctx.moveTo(xLine,0);
+  ctx.lineTo(xLine-5,5);
+  ctx.moveTo(xLine,0);
+  ctx.lineTo(xLine+5,5);
+  ctx.stroke();
+  
+  for (i = 0; i < px.length; i++) {
+     ctx.beginPath();
+     ctx.arc(xLine,px[i],5,0,2*Math.PI);
+     ctx.fillStyle = "green";
+     ctx.fill();
+     ctx.stroke();
+  }
+}
