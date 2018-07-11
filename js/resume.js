47331e83b4cafb729ef4eb50900d62eb4ea10783
Jonathan <gamma_chen@yahoo.com.tw>
2018-07-11 19:13:36 +0800

    Hisilcon career

--- js/resume.js
+++ js/resume.js
@@ -246,7 +246,7 @@ function createWorkContent() {
   str[3] = new Array(1);
   str[4] = new Array(1);
   str[5] = new Array(1);
-  str[0][0] = "glsl frontend + LLVM backend compiler development for a whole new GPU";
+  str[0][0] = "glsl frontend + LLVM backend compiler development for a whole new GPU in Hisilcon";
   str[1][0] = "Web and document software study and design (html/css/javascript, Graphivz, ...)";
   str[2][0] = "Senior software engineer in Marvell       (lvm open source team at my personal time)";
   str[2][1] = "  llvm optimization for ARM";
